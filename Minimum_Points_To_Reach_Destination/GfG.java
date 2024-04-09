283eabe11b6a21cfa29cf199086805d7e41c81d8
Anish Gupta <anishgupta675@gmail.com>
2024-04-10 00:00:13 +0530

    Minimum Points To Reach Destination

---
+++ Minimum_Points_To_Reach_Destination/GfG.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GfG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int m = sc.nextInt();
+            int n = sc.nextInt();
+            int points[][] = new int[m][n];
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++) points[i][j] = sc.nextInt();
+            Solution ob = new Solution();
+            System.out.println(ob.minPoints(points, m, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int minPoints(int points[][], int m, int n) {
+        // Your code goes here
+        int[][] dp = new int[m][n];
+        for(int i = m - 1; i >= 0; --i) {
+            for(int j = n - 1; j >= 0; --j) {
+                if(i == m - 1 && j == n - 1) dp[i][j] = Math.min(0, points[i][j]);
+                else if(i == m - 1) dp[i][j] = Math.min(0, points[i][j] + dp[i][j + 1]);
+                else if(j == n - 1) dp[i][j] = Math.min(0, points[i][j] + dp[i + 1][j]);
+                else dp[i][j] = Math.min(0, points[i][j] + Math.max(dp[i][j + 1], dp[i + 1][j]));
+            }
+        }
+        return Math.abs(dp[0][0]) + 1;
+    }
+}
