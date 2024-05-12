ae1c363f8a62efc47a9db8874f6c5a7b658b77d1
Anish Gupta <anishgupta675@gmail.com>
2024-05-12 12:15:03 +0530

    Check if it is possible to survive on Island

---
+++ Check_if_it_is_possible_to_survive_on_Island/main.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int minimumDays(int S, int N, int M){
+        // code here
+        double req = S * M;
+        if((S - S / 7) * N < req) return -1;
+        else return ceil(req / N);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int S, N, M;
+        cin>> S >> N >> M;
+        
+        Solution ob;
+        cout<<ob.minimumDays(S, N, M)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
