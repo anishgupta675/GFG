86b5ab0bf0800c67aaa12ab56e47c2a96c2dc9ad
Anish Gupta <anishgupta675@gmail.com>
2024-05-12 12:15:03 +0530

    Check if it is possible to survive on Island

---
+++ Check_if_it_is_possible_to_survive_on_Island/GFG.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0) {
+            String arr[] = in.readLine().trim().split("\\s+");
+            int S = Integer.parseInt(arr[0]);
+            int N = Integer.parseInt(arr[1]);
+            int M = Integer.parseInt(arr[2]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.minimumDays(S, N, M));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int minimumDays(int S, int N, int M){
+        // code here
+        double req = S * M;
+        if((S - S / 7) * N < req) return -1;
+        else return (int)Math.ceil(req / N);
+    }
+}
