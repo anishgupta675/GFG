da47dcf42a18d62f192819f8a327da40acad8a1b
Anish Gupta <anishgupta675@gmail.com>
2024-12-30 08:46:35 +0000

    Union of Arrays with Duplicates

---
+++ Union_of_Arrays_with_Duplicates/main.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+
+class Solution:    
+    #Function to return the count of number of elements in union of two arrays.
+    def findUnion(self, a, b):
+        # code here
+        set1, set2 = set(a), set(b)
+        return len(list(set1 | set2))
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+
+        a = [int(x) for x in input().strip().split()]
+        b = [int(x) for x in input().strip().split()]
+        ob = Solution()
+
+        print(ob.findUnion(a, b))
+        print("~")
+
+# } Driver Code Ends
