1ef2f168819cb825e8f86acd6c6495eeeee23e9b
Anish Gupta <anishgupta675@gmail.com>
2025-01-05 10:50:33 +0000

    Count Pairs whose sum is less than target

---
+++ Count_Pairs_whose_sum_is_less_than_target/main.py
@@ -0,0 +1,43 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+# } Driver Code Ends
+#User function Template for python3
+class Solution:
+    #Complete the below function
+    def countPairs(self, arr, target):
+        #Your code here
+        arr.sort()
+        ans, l, r = 0, 0, len(arr) - 1
+        while l < r:
+            while r > l and arr[l] + arr[r] >= target:
+                r -= 1
+            ans += r - l
+            l += 1
+        return ans
+
+
+#{ 
+ # Driver Code Starts.
+
+def main():
+    T = int(input())
+    while (T > 0):
+
+        A = [int(x) for x in input().strip().split()]
+
+        k = int(input())
+        ob = Solution()
+        print(ob.countPairs(A, k))
+        print('~')
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
