1dc9b4e99eda210da7dc4401fe11837197822eb8
Anish Gupta <anishgupta675@gmail.com>
2025-08-15 12:10:35 +0530

    Insert Interval

---
+++ Insert_Interval/main.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> insertInterval(vector<vector<int>> &intervals,
+                                        vector<int> &newInterval) {
+        // code here
+        vector<vector<int>> ans;
+        int index = 0;
+        const int n = intervals.size();
+        
+        while (index < n) {
+            if (intervals[index][1] >= newInterval[0]) break;
+            ans.push_back(intervals[index++]);
+        }
+        
+        if (index == n) {
+            ans.push_back(newInterval);
+            return ans;
+        }
+        
+        newInterval[0] = min(newInterval[0], intervals[index][0]);
+        
+        while (index < n) {
+            if (intervals[index][0] > newInterval[1]) break;
+            newInterval[1] = max(newInterval[1], intervals[index][1]);
+            index++;
+        }
+        
+        ans.push_back(newInterval);
+        
+        while (index < n) {
+            ans.push_back(intervals[index++]);
+        }
+        
+        return ans;
+    }
+};
+
+int main() {}
