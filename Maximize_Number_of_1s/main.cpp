cff51ab7cd450a4faec829f1686b17c896f3de04
Anish Gupta <anishgupta675@gmail.com>
2025-08-28 23:30:02 +0530

    Maximize Number of 1s

---
+++ Maximize_Number_of_1s/main.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxOnes(vector<int>& arr, int k) {
+        // code here
+        int n=arr.size();
+        int i=0,j=0,count=0,maxi=0;
+        while(j<n){
+            if(arr[j]==0) count++;
+            while(count>k){
+                if(arr[i]==0) count--;
+                i++;
+            }
+            maxi=max(maxi,j-i+1);
+            j++;
+        }
+        return maxi;
+    }
+};
+
+int main() {}
