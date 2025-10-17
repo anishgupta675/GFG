7aa8f65e9a6d5e0a2914a89f729fd9adea7538ec
Anish Gupta <anishgupta675@gmail.com>
2025-10-17 23:38:58 +0530

    BST to greater sum tree

---
+++ BST_to_greater_sum_tree/main.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+class Solution {
+    int cs = 0;
+  public:
+    void transformTree(Node *root) {
+        // code here
+        if(!root)return;
+        int d = root->data;
+        transformTree(root->right);
+        root->data = cs;
+        cs += d;
+        transformTree(root->left);
+    }
+};
+
+int main() {}
