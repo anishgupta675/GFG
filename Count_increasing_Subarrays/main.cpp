1e2066cd6fd215b5663359a506f2bd70344315e5
Anish Gupta <anishgupta675@gmail.com>
2026-04-11 23:57:34 +0530

    Count Increasing Subarrays

---
+++ Count_increasing_Subarrays/main.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int countIncreasing(vector<int>& arr) {
+        // code here.
+        int n=arr.size();
+        if(n<2) return 0;
+        int left=0, right=1, cnt=0;
+        while(right<n){
+            if(arr[right-1]<arr[right])
+                cnt+=(right-left);
+            else
+                left=right;
+            right++;
+        }
+        return cnt;
+    }
+};
+
+int main() {}
