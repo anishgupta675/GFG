c5c662c7037e7a29a59c5d4e343ca4394315207e
Anish Gupta <anishgupta675@gmail.com>
2026-04-19 16:09:12 +0530

    Check for Power

---
+++ Check_for_Power/main.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    bool isPower(int x, int y) {
+        // code here
+        if(x == 1){
+            return y == 1;
+        }
+        while(y%x == 0){
+            y=y/x ;
+        }
+        return y==1;
+    }
+};
+
+int main() {}
